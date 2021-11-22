6de2ed641db921c2ce5dcbeedb34ecb1f64fe2f3
emmanoel <emmanoelporto@gmail.com>
2021-11-22 05:25:52 -0300

    05: Search Page : create Input text and Button

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+      disabled: true,
+    };
+    this.searchHandle = this.searchHandle.bind(this);
+  }
+
+  searchHandle({ target }) {
+    const { searchInput } = this.state;
+    this.setState({
+      searchInput: target.value,
+    });
+
+    if (searchInput.length + 1 >= 2) {
+      this.setState({
+        disabled: false,
+      });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
-        <p>olá</p>
+        <input
+          type="text"
+          onChange={ this.searchHandle }
+          data-testid="search-artist-input"
+        />
+
+        <button
+          type="submit"
+          disabled={ disabled }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
